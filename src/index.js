1531b563efa2724b424b19206129480b75baa181
KostiantynZhulinas <k.zhulinas@gmai.com>
2023-09-05 21:59:13 +0300

    add files

---
+++ src/index.js
@@ -0,0 +1,26 @@
+function isPrime(number) {
+  if (number <= 1) {
+      return false;
+  }
+  if (number <= 3) {
+      return true;
+  }
+  if (number % 2 === 0 || number % 3 === 0) {
+      return false;
+  }
+  let i = 5;
+  while (i * i <= number) {
+      if (number % i === 0 || number % (i + 2) === 0) {
+          return false;
+      }
+      i += 6;
+  }
+  return true;
+}
+let number = prompt('Enter number');
+
+if (isPrime(number)) {
+    console.log(`Число ${number} є простим числом`);
+} else {
+    console.log(`Число ${number} не є простим числом`);
+}
